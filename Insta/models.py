dfec217bd3008d8044223777c6cf5eb51754a96a
Younggy <yanggy1613_jlu@163.com>
2019-07-27 10:47:15 +0800

    CRUD finish

--- Insta/models.py
+++ Insta/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from imagekit.models import ProcessedImageField
+from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
     title = models.TextField(blank=True, null=True)
@@ -8,3 +9,6 @@ class Post(models.Model):
         format = 'JPEG',
         blank = True,
         null = True,)
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
--- Insta/views.py
+++ Insta/views.py
@@ -1,6 +1,11 @@
 
 from django.views.generic import TemplateView, ListView, DetailView
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from Insta.models import Post
+
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -16,3 +21,18 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "make_post.html"
+    fields = '__all__'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = "update_post.html"
+    fields = ('title',)
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "delete_post.html"
+    success_url = reverse_lazy('home')
